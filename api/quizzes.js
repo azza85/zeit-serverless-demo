8d4d9ecc2d143f00d13ad1bac87075e42daf6498
azza85 <aaronkenez@gmail.com>
2020-09-22 12:24:45 +1000

    add in new file

---
+++ api/quizzes.js
@@ -0,0 +1,331 @@
+module.exports = (req, res) => {
+  res.status(200).send([
+    {
+      id: 1,
+      name: "FGP Feeds",
+      date: "2020-09-11T17:01:00+10:00",
+      img: "img5f59cf4bee095.png",
+      img_title: "THERE",
+      questions: {
+        2: {
+          id: 2,
+          name: "Who is this?",
+          options: {
+            1: {id: 1, name: "Me", answers: 0, correct: 0},
+            2: {id: 2, name: "Tom", answers: 0, correct: 0},
+            3: {id: 3, name: "TR", answers: 100, correct: 1},
+            4: {id: 4, name: "None", answers: 0, correct: 0},
+          },
+        },
+        3: {
+          id: 3,
+          name: "Who is this?",
+          options: {
+            5: {id: 5, name: "Me", answers: 50, correct: 1},
+            6: {id: 6, name: "Tom", answers: 0, correct: 0},
+            7: {id: 7, name: "TR", answers: 50, correct: 0},
+            8: {id: 8, name: "None", answers: 0, correct: 0},
+          },
+        },
+        4: {
+          id: 4,
+          name: "Who is this?",
+          options: {
+            9: {id: 9, name: "Me", answers: 0, correct: 1},
+            10: {id: 10, name: "Tom", answers: 0, correct: 0},
+            11: {id: 11, name: "TR", answers: 50, correct: 0},
+            12: {id: 12, name: "None", answers: 50, correct: 0},
+          },
+        },
+        5: {
+          id: 5,
+          name: "Who is this?",
+          options: {
+            13: {id: 13, name: "Me", answers: 0, correct: 0},
+            14: {id: 14, name: "Tom", answers: 50, correct: 0},
+            15: {id: 15, name: "TR", answers: 50, correct: 1},
+            16: {id: 16, name: "None", answers: 0, correct: 0},
+          },
+        },
+        6: {
+          id: 6,
+          name: "Who is this?",
+          options: {
+            17: {id: 17, name: "Me", answers: 50, correct: 1},
+            18: {id: 18, name: "Tom", answers: 0, correct: 0},
+            19: {id: 19, name: "TR", answers: 50, correct: 0},
+            20: {id: 20, name: "None", answers: 0, correct: 0},
+          },
+        },
+        7: {
+          id: 7,
+          name: "Who is this?",
+          options: {
+            21: {id: 21, name: "Me", answers: 50, correct: 0},
+            22: {id: 22, name: "Tom", answers: 0, correct: 0},
+            23: {id: 23, name: "TR", answers: 50, correct: 0},
+            24: {id: 24, name: "None", answers: 0, correct: 1},
+          },
+        },
+        8: {
+          id: 8,
+          name: "Who is this?",
+          options: {
+            25: {id: 25, name: "Me", answers: 0, correct: 0},
+            26: {id: 26, name: "Tom", answers: 50, correct: 0},
+            27: {id: 27, name: "TR", answers: 50, correct: 0},
+            28: {id: 28, name: "None", answers: 0, correct: 1},
+          },
+        },
+        9: {
+          id: 9,
+          name: "Who is this?",
+          options: {
+            29: {id: 29, name: "Me", answers: 0, correct: 0},
+            30: {id: 30, name: "Tom", answers: 0, correct: 0},
+            31: {id: 31, name: "TR", answers: 100, correct: 1},
+            32: {id: 32, name: "None", answers: 0, correct: 0},
+          },
+        },
+        10: {
+          id: 10,
+          name: "Who is this?",
+          options: {
+            33: {id: 33, name: "Me", answers: 50, correct: 1},
+            34: {id: 34, name: "Tom", answers: 0, correct: 0},
+            35: {id: 35, name: "TR", answers: 50, correct: 0},
+            36: {id: 36, name: "None", answers: 0, correct: 0},
+          },
+        },
+        11: {
+          id: 11,
+          name: "Who is this?",
+          options: {
+            37: {id: 37, name: "Me", answers: 0, correct: 0},
+            38: {id: 38, name: "Tom", answers: 0, correct: 1},
+            39: {id: 39, name: "TR", answers: 50, correct: 0},
+            40: {id: 40, name: "None", answers: 50, correct: 0},
+          },
+        },
+      },
+    },
+    {
+      id: 2,
+      name: "FGP UAT builds",
+      date: "2020-09-19T11:59:00+10:00",
+      img: "img5f6414b38096d.png",
+      img_title: "THERE",
+      questions: {
+        12: {
+          id: 12,
+          name: "Who is this?",
+          options: {
+            41: {id: 41, name: "Me", answers: 50, correct: 1},
+            42: {id: 42, name: "Tom", answers: 50, correct: 0},
+            43: {id: 43, name: "TR", answers: 0, correct: 0},
+            44: {id: 44, name: "None", answers: 0, correct: 0},
+          },
+        },
+        13: {
+          id: 13,
+          name: "Who is this?",
+          options: {
+            45: {id: 45, name: "Me", answers: 50, correct: 0},
+            46: {id: 46, name: "Tom", answers: 50, correct: 1},
+            47: {id: 47, name: "TR", answers: 0, correct: 0},
+            48: {id: 48, name: "None", answers: 0, correct: 0},
+          },
+        },
+        14: {
+          id: 14,
+          name: "Who is this?",
+          options: {
+            49: {id: 49, name: "Me", answers: 50, correct: 0},
+            50: {id: 50, name: "Tom", answers: 50, correct: 0},
+            51: {id: 51, name: "TR", answers: 0, correct: 1},
+            52: {id: 52, name: "None", answers: 0, correct: 0},
+          },
+        },
+        15: {
+          id: 15,
+          name: "Who is this?",
+          options: {
+            53: {id: 53, name: "Me", answers: 50, correct: 0},
+            54: {id: 54, name: "Tom", answers: 50, correct: 0},
+            55: {id: 55, name: "TR", answers: 0, correct: 0},
+            56: {id: 56, name: "None", answers: 0, correct: 1},
+          },
+        },
+        16: {
+          id: 16,
+          name: "Who is this?",
+          options: {
+            57: {id: 57, name: "Me", answers: 50, correct: 0},
+            58: {id: 58, name: "Tom", answers: 50, correct: 0},
+            59: {id: 59, name: "TR", answers: 0, correct: 0},
+            60: {id: 60, name: "None", answers: 0, correct: 1},
+          },
+        },
+        17: {
+          id: 17,
+          name: "Who is this?",
+          options: {
+            61: {id: 61, name: "Me", answers: 50, correct: 0},
+            62: {id: 62, name: "Tom", answers: 50, correct: 0},
+            63: {id: 63, name: "TR", answers: 0, correct: 1},
+            64: {id: 64, name: "None", answers: 0, correct: 0},
+          },
+        },
+        18: {
+          id: 18,
+          name: "Who is this?",
+          options: {
+            65: {id: 65, name: "Me", answers: 50, correct: 0},
+            66: {id: 66, name: "Tom", answers: 50, correct: 1},
+            67: {id: 67, name: "TR", answers: 0, correct: 0},
+            68: {id: 68, name: "None", answers: 0, correct: 0},
+          },
+        },
+        19: {
+          id: 19,
+          name: "Who is this?",
+          options: {
+            69: {id: 69, name: "Me", answers: 50, correct: 1},
+            70: {id: 70, name: "Tom", answers: 50, correct: 0},
+            71: {id: 71, name: "TR", answers: 0, correct: 0},
+            72: {id: 72, name: "None", answers: 0, correct: 0},
+          },
+        },
+        20: {
+          id: 20,
+          name: "Who is this?",
+          options: {
+            73: {id: 73, name: "Me", answers: 50, correct: 1},
+            74: {id: 74, name: "Tom", answers: 50, correct: 0},
+            75: {id: 75, name: "TR", answers: 0, correct: 0},
+            76: {id: 76, name: "None", answers: 0, correct: 0},
+          },
+        },
+        21: {
+          id: 21,
+          name: "Who is this?",
+          options: {
+            77: {id: 77, name: "Me", answers: 50, correct: 1},
+            78: {id: 78, name: "Tom", answers: 50, correct: 0},
+            79: {id: 79, name: "TR", answers: 0, correct: 0},
+            80: {id: 80, name: "None", answers: 0, correct: 0},
+          },
+        },
+      },
+    },
+    {
+      id: 4,
+      name: "Felix Koehler",
+      date: "2020-09-19T15:24:00+10:00",
+      img: "img5f64448cf320b.png",
+      img_title: "TOM",
+      questions: {
+        23: {
+          id: 23,
+          name: "Who is this?",
+          options: {
+            86: {id: 86, name: "Me", answers: 0, correct: 0},
+            87: {id: 87, name: "Tom", answers: 0, correct: 1},
+            88: {id: 88, name: "TR", answers: 0, correct: 0},
+            89: {id: 89, name: "None", answers: 0, correct: 0},
+          },
+        },
+        24: {
+          id: 24,
+          name: "Who is this?",
+          options: {
+            90: {id: 90, name: "Me", answers: 0, correct: 1},
+            91: {id: 91, name: "Tom", answers: 0, correct: 0},
+            92: {id: 92, name: "TR", answers: 0, correct: 0},
+            93: {id: 93, name: "None", answers: 0, correct: 0},
+          },
+        },
+        25: {
+          id: 25,
+          name: "Who is this?",
+          options: {
+            94: {id: 94, name: "Me", answers: 0, correct: 0},
+            95: {id: 95, name: "Tom", answers: 0, correct: 0},
+            96: {id: 96, name: "TR", answers: 0, correct: 1},
+            97: {id: 97, name: "None", answers: 0, correct: 0},
+          },
+        },
+        26: {
+          id: 26,
+          name: "Who is this?",
+          options: {
+            98: {id: 98, name: "Me", answers: 0, correct: 0},
+            99: {id: 99, name: "Tom", answers: 0, correct: 0},
+            100: {id: 100, name: "TR", answers: 0, correct: 0},
+            101: {id: 101, name: "None", answers: 0, correct: 1},
+          },
+        },
+        27: {
+          id: 27,
+          name: "Who is this?",
+          options: {
+            102: {id: 102, name: "Me", answers: 0, correct: 0},
+            103: {id: 103, name: "Tom", answers: 0, correct: 1},
+            104: {id: 104, name: "TR", answers: 0, correct: 0},
+            105: {id: 105, name: "None", answers: 0, correct: 0},
+          },
+        },
+        28: {
+          id: 28,
+          name: "Who is this?",
+          options: {
+            106: {id: 106, name: "Me", answers: 0, correct: 0},
+            107: {id: 107, name: "Tom", answers: 0, correct: 0},
+            108: {id: 108, name: "TR", answers: 0, correct: 1},
+            109: {id: 109, name: "None", answers: 0, correct: 0},
+          },
+        },
+        29: {
+          id: 29,
+          name: "Who is this?",
+          options: {
+            110: {id: 110, name: "Me", answers: 0, correct: 0},
+            111: {id: 111, name: "Tom", answers: 0, correct: 0},
+            112: {id: 112, name: "TR", answers: 0, correct: 0},
+            113: {id: 113, name: "None", answers: 0, correct: 1},
+          },
+        },
+        30: {
+          id: 30,
+          name: "Who is this?",
+          options: {
+            114: {id: 114, name: "Me", answers: 0, correct: 1},
+            115: {id: 115, name: "Tom", answers: 0, correct: 0},
+            116: {id: 116, name: "TR", answers: 0, correct: 0},
+            117: {id: 117, name: "None", answers: 0, correct: 0},
+          },
+        },
+        31: {
+          id: 31,
+          name: "Who is this?",
+          options: {
+            118: {id: 118, name: "Me", answers: 0, correct: 0},
+            119: {id: 119, name: "Tom", answers: 0, correct: 1},
+            120: {id: 120, name: "TR", answers: 0, correct: 0},
+            121: {id: 121, name: "None", answers: 0, correct: 0},
+          },
+        },
+        34: {
+          id: 34,
+          name: "Who is this?",
+          options: {
+            130: {id: 130, name: "Me", answers: 0, correct: 0},
+            131: {id: 131, name: "Tom", answers: 0, correct: 0},
+            132: {id: 132, name: "TR", answers: 0, correct: 0},
+            133: {id: 133, name: "None", answers: 0, correct: 1},
+          },
+        },
+      },
+    },
+  ]);
+};
